8b39917be1c5f7cddd542fbbe616efc026c4f28d
Diego Canevaro <diego.124z@hotmail.com>
2021-09-14 18:57:31 -0500

    mvc PC2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PC2.Models;
+
+namespace PC2.Controllers
+{
+   public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Formulario(string nombre, string apellido_paterno, string apellido_materno, string tipo_mascota, string descripcion, string telefono_contacto, string distrito, string direccion )
+        {
+            
+            return RedirectToAction("Confirmacion");
+        }
+        
+         public IActionResult Confirmacion()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
